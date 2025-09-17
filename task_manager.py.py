01c0e014621396852dee3a28d4ec62928674fcf9
davidrmpy <davidrmfitz@gmail.com>
2025-09-17 19:52:18 +0200

    Add files via upload

---
+++ task_manager.py.py
@@ -0,0 +1,11 @@
+tasks = {"Email": "High", "Write": "Low", "Meeting": "High", "Sport": "Medium"}
+
+for task, priority in tasks.items():
+    if priority == "High":
+        print(f"{task} is High priority")
+
+print(tasks)
+
+
+
+
